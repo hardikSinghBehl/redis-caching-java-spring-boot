ab057ebaf09517a86868fb084d71f33562450b8e
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-07-03 11:09:42 +0530

    minor fix: adding annotation to enable caching in project

--- src/main/java/com/hardik/bojack/RedisCachingSpringBootApplication.java
+++ src/main/java/com/hardik/bojack/RedisCachingSpringBootApplication.java
@@ -2,8 +2,10 @@ package com.hardik.bojack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class RedisCachingSpringBootApplication {
 
 	public static void main(String[] args) {
