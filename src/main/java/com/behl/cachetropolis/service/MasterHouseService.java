e627b0306d738692766b44177e50885f58841c41
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2023-11-21 13:12:37 +0530

    adding javadocs in service layer

--- src/main/java/com/behl/cachetropolis/service/MasterHouseService.java
+++ src/main/java/com/behl/cachetropolis/service/MasterHouseService.java
@@ -24,6 +24,14 @@ public class MasterHouseService {
 	private final WizardRepository wizardRepository;
 	private final MasterHouseRepository masterHouseRepository;
 
+	/**
+	 * Retrieves list of all house records stored in the datasource. The house
+	 * records are populated using flyway migration script, ensuring that the
+	 * response should never be {@code null} or {@code empty}.
+	 *
+	 * @see <a href="file:src/main/resources/db/migration/V002__adding_data_in_master_houses.sql">Flyway migrations</a>
+	 * @return List of {@link HouseDto}.
+	 */
 	public List<HouseDto> retrieve() {
 		return masterHouseRepository.findAll()
 				.stream()
@@ -31,11 +39,30 @@ public class MasterHouseService {
 				.toList();
 	}
 	
+    /**
+     * Retrieves details of a random house from the datasource.
+     *
+     * @return {@link HouseDto} representing the details of a randomly selected house.
+     */
 	public HouseDto retrieveRandom() {
 		final var house = masterHouseRepository.fetchRandom().orElseThrow(IllegalStateException::new);
 		return modelMapper.map(house, HouseDto.class);
 	}
 
+	/**
+	 * Retrieves a list of wizard records associated with the provided house ID.
+	 * Post initial datasource retrieval, the response is stored in the provisioned
+	 * cache. For all subsequent calls with the same {@code houseId}, the records
+	 * are fetched from the cache itself, reducing network calls and improving
+	 * latency.
+	 *
+	 * @param houseId The ID of the house against which wizard records are to be retrieved.
+	 * @return List of {@link WizardDto} representing the details of wizards
+	 *         associated with the provided house ID.
+	 * @throws IllegalArgumentException if houseId is {@code null}
+	 * @throws InvalidHouseIdException  if no house or wizard records exist
+	 *                                  corresponding to provided house ID.
+	 */
 	@Cacheable(value = "wizards", key = "#houseId")
 	public List<WizardDto> retrieveWizardsByHouseId(@NonNull final UUID houseId) {
 		final var wizards = wizardRepository.findByHouseId(houseId);
--- src/main/java/com/behl/cachetropolis/service/WizardService.java
+++ src/main/java/com/behl/cachetropolis/service/WizardService.java
@@ -22,6 +22,14 @@ import com.behl.cachetropolis.repository.WizardRepository;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
+/**
+ * Service class responsible for performing CRUD operations on {@link Wizard}
+ * entity. Caching mechanisms are employed to improve latency and reduce network
+ * calls. It maintains strong read consistency and ensures the provisioned cache
+ * is synced with the datasource.  
+ * 
+ * @see com.behl.cachetropolis.configuration.RedisConfiguration
+ */
 @Service
 @RequiredArgsConstructor
 public class WizardService {
@@ -30,6 +38,18 @@ public class WizardService {
 	private final CacheManager listCacheManager;
 	private final WizardRepository wizardRepository;
 
+	/**
+	 * Creates a new wizard record corresponding to provided details in {@link WizardCreationRequestDto}
+	 * On successful save operation, the list of wizard records corresponding to the house-id 
+	 * of the newly created wizard record is cleared from the provisioned cache to maintain 
+	 * strong read consistency.
+	 * 
+	 * @param wizardCreationRequest The request for creating a new wizard (must not be {@code null})
+	 * @return {@code Non null} primary identifier ({@link UUID}) of the newly created wizard record.
+	 * @throws IllegalArgumentException if the provided wizardCreationRequest is {@code null}
+	 * @throws InvalidHouseIdException if no house record exists in the datasource
+	 *                                  corresponding to given house-id in the request.
+	 */
 	@CacheEvict(value = "wizards", key = "#wizardCreationRequest.houseId")
 	public UUID create(@NonNull final WizardCreationRequestDto wizardCreationRequest) {
 		final var wizard = modelMapper.map(wizardCreationRequest, Wizard.class);
@@ -45,7 +65,24 @@ public class WizardService {
 		}
 		return savedWizard.getId();
 	}
-	     
+	    
+	/**
+	 * Updates wizard record by its ID. Post record updation in the datasource, the
+	 * wizard record is also updated in the provisioned cache if present.
+	 * Additionally, the list of wizard records corresponding to the house-id of the
+	 * updated wizard is evicted from the cache to maintain strong read consistency.
+	 * 
+	 * This method is designed as a partial update (PATCH) where only the specified
+	 * field(s) in the {@link WizardUpdationRequestDto} are updated. {@code null}
+	 * field(s) are ignored. 
+	 *
+	 * @param wizardId The Id of the wizard to be updated. (must not be {@code null})
+	 * @param WizardUpdationRequest containing the field(s) to be updated. (must not be {@code null})
+	 * @return {@link WizardDto} representing the updated wizard record.
+	 * @throws IllegalArgumentException if any of the parameter provided is {@code null}
+	 * @throws InvalidWizardIdException if no wizard record exists in the datasource
+	 *                                  corresponding to the given wizardId.
+	 */
 	@CachePut(value = "wizard", key = "#wizardId", cacheManager = "objectCacheManager")
 	public WizardDto update(@NonNull final UUID wizardId, @NonNull final WizardUpdationRequestDto WizardUpdationRequest) {		
 		final var wizard = getWizardById(wizardId);
@@ -56,12 +93,35 @@ public class WizardService {
 		return modelMapper.map(updatedWizard, WizardDto.class);
 	}
 
+	/**
+	 * Retrieves a {@code non null} {@link WizardDto} by its ID. Post initial datasource
+	 * retrieval, the response is stored in the provisioned cache. For all subsequent 
+	 * calls with the same {@code wizardId}, the record is fetched from the cache itself, 
+	 * reducing network calls and improving latency.
+	 *
+	 * @param wizardId The id of the wizard record to be retrieved. (must not be {@code null})
+	 * @return {@link WizardDto} representing the details of the retrieved wizard.
+	 * @throws IllegalArgumentException if the provided wizardId is {@code null}
+	 * @throws InvalidWizardIdException if no wizard record exists in the datasource
+	 *                                  corresponding to given wizardId.
+	 */
 	@Cacheable(value = "wizard", key = "#wizardId", cacheManager = "objectCacheManager")
 	public WizardDto retrieveById(@NonNull final UUID wizardId) {
 		final var retrievedWizard = getWizardById(wizardId);
 		return modelMapper.map(retrievedWizard, WizardDto.class);
 	}
 
+	/**
+	 * Deletes a wizard record by its ID. In addition to record deletion from
+	 * datasource, the wizard record is also cleared from the provisioned cache.
+	 * List of wizard records corresponding to the house-id of the deleted wizard
+	 * are also cleared from the cache to maintain strong read consistency.
+	 *
+	 * @param wizardId The Id of the wizard to be deleted. (must not be {@code null})
+	 * @throws IllegalArgumentException if the provided wizardId is {@code null}
+	 * @throws InvalidWizardIdException if no wizard record exists in the datasource
+	 *                                  corresponding to given wizardId.
+	 */
 	@CacheEvict(value = "wizard", key = "#wizardId", cacheManager = "objectCacheManager")
 	public void delete(@NonNull final UUID wizardId) {
 		final var wizard = getWizardById(wizardId);
@@ -69,6 +129,16 @@ public class WizardService {
 		listCacheManager.getCache("wizards").evict(wizard.getHouseId());;
 	}
 	
+	/**
+	 * Retrieves a {@link Wizard} record by its ID. The returned entity will never
+	 * be null.
+	 *
+	 * @param wizardId The id of the wizard record to be retrieved. (must not be {@code null})
+	 * @return {@link Wizard} entity representing the retrieved wizard record from the datasource.
+	 * @throws IllegalArgumentException if the provided wizardId is {@code null}
+	 * @throws InvalidWizardIdException if no wizard record exists in the datasource
+	 *                                  corresponding to given wizardId.
+	 */
 	private Wizard getWizardById(@NonNull final UUID wizardId) {
 		return wizardRepository.findById(wizardId).orElseThrow(InvalidWizardIdException::new);
 	}
